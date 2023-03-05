4ef15ee90c9c2b3be87d68b3df76b70ac558980e
Swhag <khg5293@outlook.com>
2023-03-05 02:16:27 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import Weather from './components/Weather';
+
+import useWeatherData from './utils/useWeatherData';
+import Dashboards from './components/Dashboards';
+import DailyForecast from './components/DailyForecast';
 
 function App() {
+  const [location, setLocation] = useState('New York');
+  const weatherData = useWeatherData(location);
+
   return (
     <div className='main-container'>
       <Navbar />
-      <Weather />
+      <div className='weather-container'>
+        <Dashboards
+          weatherData={weatherData}
+          location={location}
+          setLocation={setLocation}
+        />
+        <DailyForecast
+          weatherData={weatherData}
+          location={location}
+          setLocation={setLocation}
+        />
+      </div>
     </div>
   );
 }
