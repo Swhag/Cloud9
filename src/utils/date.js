04184c979f1c7bfa29891e5a19c7abdc1ccd9043
Swhag <khg5293@outlook.com>
2023-03-05 20:50:34 -0500

    Update date.js

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,3 +1,13 @@
+/**
+ * Formats a date object into an array of weekday and month/day strings.
+ *
+ * @param {Date} date - The date object to format.
+ * @returns {Array} An array containing the weekday and month/day strings.
+ *
+ * @example
+ * formatDate(new Date('2022-03-06')) // Returns ['Sunday', 'Mar 6']
+ */
+
 function formatDate(date) {
   const options = { weekday: 'long', month: 'short', day: 'numeric' };
   const dateString = date.toLocaleDateString(undefined, options);
