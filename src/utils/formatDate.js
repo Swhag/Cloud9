e0b10716985356d22c43a17432405b347c00378c
Swhag <khg5293@outlook.com>
2023-03-05 20:53:38 -0500

    utils formatDate.js updated with comments

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -15,4 +15,21 @@ function formatDate(date) {
   return [weekday.trim(), monthDay.trim()];
 }
 
-export { formatDate };
+/**
+ * Formats a Unix timestamp into a string representing the time of day,
+ * formatted according to the options specified.
+ *
+ * @param {number} timestamp - The Unix timestamp to format.
+ * @returns {string} A string representing the formatted time.
+ *
+ * @example
+ * formatTime(1646318526) // Returns '07:28 AM'
+ */
+function formatTime(timestamp) {
+  return new Date(timestamp * 1000).toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+}
+
+export { formatDate, formatTime };
