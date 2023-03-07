1b4c1596579b8e8ab7f452c3a711bbeec5ecd2d1
Swhag <khg5293@outlook.com>
2023-03-07 01:43:17 -0500

    Update weatherApi.js

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -1,13 +1,20 @@
 import axios from 'axios';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY1 = process.env.REACT_APP_API_KEY1;
 
-const buildGeocodingUrl = (location) => {
-  return `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${API_KEY}`;
+const buildLocationUrl = (location) => {
+  return `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${API_KEY1}`;
 };
 
-const buildWeatherUrl = (lat, lon) => {
-  return `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&units=imperial&appid=${API_KEY}`;
+export const getLocationData = async (location) => {
+  const LocationUrl = buildLocationUrl(location);
+  const LocationResponse = await axios.get(LocationUrl);
+  return LocationResponse.data;
+};
+
+const buildGeocodingUrl = (location) => {
+  return `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${API_KEY1}`;
 };
 
 export const getGeocodingData = async (location) => {
@@ -16,14 +23,12 @@ export const getGeocodingData = async (location) => {
   return geocodingResponse.data[0];
 };
 
+const buildWeatherUrl = (lat, lon) => {
+  return `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&units=imperial&appid=${API_KEY}`;
+};
+
 export const getWeatherData = async (lat, lon) => {
   const weatherUrl = buildWeatherUrl(lat, lon);
   const weatherResponse = await axios.get(weatherUrl);
   return weatherResponse.data;
 };
-
-// export const getWeatherData = async (lat, lon) => {
-//   const forecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=imperial&appid=${API_KEY}`;
-//   const forecastResponse = await axios.get(forecastUrl);
-//   return forecastResponse.data.daily;
-// };
