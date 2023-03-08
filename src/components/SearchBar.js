7a8fb7e869f169642d0b014b6253ada9fb1650c7
Swhag <khg5293@outlook.com>
2023-03-07 22:48:22 -0500

    react select implemented to show suggestion

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,30 +1,56 @@
 import React, { useState } from 'react';
 import { capitalize } from '../utils/formatUtils';
+import { getLocationData } from '../utils/weatherAPI';
+import Select from 'react-select';
+import { components } from 'react-select';
 
 function SearchBar(props) {
   const { setLocation } = props;
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchValue, setSearchValue] = useState('');
+  const [options, setOptions] = useState([]);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setLocation(capitalize(searchTerm));
-    setSearchTerm('');
-    console.log(searchTerm);
+
+    // ... code to fetch location data ...
+    const cityResponse = await getLocationData(capitalize(searchValue));
+    console.log('API called');
+
+    const cityOptions = cityResponse.map((city) => ({
+      label: city.state
+        ? `${city.name}, ${city.state}, ${city.country}`
+        : `${city.name}, ${city.country}`,
+      value: city.name,
+    }));
+    console.log(cityOptions);
+    setOptions(cityOptions);
+  };
+
+  const handleSelectChange = (selectedOption) => {
+    setLocation(selectedOption.value);
+    setSearchValue('');
+  };
+
+  const DropdownIndicator = (props) => {
+    return (
+      components.DropdownIndicator && (
+        <components.DropdownIndicator {...props}>
+          <i className='fa-solid fa-magnifying-glass'></i>
+        </components.DropdownIndicator>
+      )
+    );
   };
 
   return (
     <form className='search-bar' onSubmit={handleSubmit}>
-      <button type='submit'>
-        <i className='fa-solid fa-magnifying-glass'></i>
-      </button>
-
-      <input
-        type='text'
+      <Select
+        components={{ DropdownIndicator }}
+        options={options}
+        value={{ label: searchValue, value: searchValue }}
+        onChange={handleSelectChange}
+        onInputChange={setSearchValue}
         placeholder='Search Location here...'
-        name='search'
-        autoComplete='off'
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
+        noOptionsMessage={() => 'No matching locations found'}
       />
     </form>
   );
