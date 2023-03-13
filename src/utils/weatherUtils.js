e2762c155b25780bafdadcf46a95430b289ff6bb
Swhag <khg5293@outlook.com>
2023-03-13 00:45:57 -0400

    Delete weatherUtils.js

--- src/utils/weatherUtils.js
+++
@@ -1,7 +0,0 @@
-function getWindDirection(degrees) {
-  const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
-  const index = Math.round((degrees % 360) / 45);
-  return directions[index];
-}
-
-export { getWindDirection };
