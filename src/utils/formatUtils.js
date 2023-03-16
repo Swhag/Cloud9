9dea02a0989eb1509c1ce6a4e4211b07395ce18f
Swhag <khg5293@outlook.com>
2023-03-16 00:27:15 -0400

    Update formatUtils.js

--- src/utils/formatUtils.js
+++ src/utils/formatUtils.js
@@ -28,37 +28,24 @@ function formatDate(date) {
  * @example
  * formatTime(1646318526) // Returns '07:28 AM'
  */
+// function formatTime(timestamp) {
+//   return new Date(timestamp * 1000).toLocaleTimeString([], {
+//     hour: '2-digit',
+//     minute: '2-digit',
+//   });
+// }
+
 function formatTime(timestamp) {
-  return new Date(timestamp * 1000).toLocaleTimeString([], {
-    hour: '2-digit',
-    minute: '2-digit',
-  });
-}
-
-// --------------------------------------------------------
-// --------------------------------------------------------
-
-function formatTimeHourly(dt) {
-  // Convert the Unix timestamp to milliseconds and create a new Date object
-  const date = new Date(dt * 1000);
-
-  // Get the hours, minutes, and AM/PM designation from the date
+  const date = new Date(timestamp * 1000);
   let hours = date.getHours();
   const minutes = date.getMinutes();
   const ampm = hours >= 12 ? 'PM' : 'AM';
-
-  // Convert the hours from 24-hour to 12-hour format
   hours = hours % 12;
-  hours = hours ? hours : 12; // if hours is 0, set it to 12
-
-  // Pad the hours and minutes with leading zeros if necessary
-  const paddedHours = hours < 10 ? '0' + hours : hours;
-  const paddedMinutes = minutes < 10 ? '0' + minutes : minutes;
-
-  // Combine the formatted time with the AM/PM designation
-  const formattedTime = `${paddedHours}:${paddedMinutes} ${ampm}`;
-
-  // Return the formatted time string
+  hours = hours ? hours : 12; // if hours is 0, it should be 12
+  const formattedHours = hours < 10 ? `${hours}` : hours; // remove the leading zero from the hours if necessary
+  const formattedTime = `${formattedHours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${ampm}`;
   return formattedTime;
 }
 
@@ -91,11 +78,4 @@ function getWindDirection(degrees) {
 // --------------------------------------------------------
 // --------------------------------------------------------
 
-export {
-  formatDate,
-  formatTime,
-  formatTimeHourly,
-  capitalize,
-  metersToMiles,
-  getWindDirection,
-};
+export { formatDate, formatTime, capitalize, metersToMiles, getWindDirection };
