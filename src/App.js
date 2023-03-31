d28824bb8e4aaca11d0bb47ac6d450d565bcd170
Swhag <khg5293@outlook.com>
2023-03-31 01:49:39 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 
 import Navbar from './components/Navbar';
@@ -7,116 +7,93 @@ import SearchBar from './components/SearchBar';
 import SavedLocation from './pages/SavedLocation';
 import Settings from './pages/Settings';
 import Dashboards from './pages/Dashboards';
+
 import { getWeatherData } from './utils/weatherAPI';
-import { getImage } from './utils/weatherImages';
-import { setFilter } from './utils/backgroundFilter';
-import { setDashboardColor } from './utils/dashboardColor';
+import { setWeather, setLocation } from './redux/weatherSlice';
+import {
+  setShowNavbar,
+  setCurrentPage,
+  setBackgroundConfig,
+  setBackgroundFilter,
+  setDashboardStyle,
+} from './redux/componentStylesSlice';
 
 function App() {
-  const [location, setLocation] = useState('Manhattan, NY');
-  const [weatherData, setWeatherData] = useState([]);
-  const [showNavbar, setShowNavbar] = useState(false);
-  const [currentPage, setCurrentPage] = useState('dashboard');
-  const [lat, setLat] = useState(40.7127);
-  const [lon, setLon] = useState(-74.006);
-  const [nav, setNav] = useState('show');
+  const dispatch = useDispatch();
+  const { weatherData, location, lat, lon } = useSelector(
+    (state) => state.weather
+  );
+  const {
+    showNavbar,
+    currentPage,
+    backgroundConfig,
+    backgroundFilter,
+    dashboardStyle,
+  } = useSelector((state) => state.componentStyles);
   const { autoSync, syncFrequency, unit, dynamicBackground } = useSelector(
     (state) => state.settings
   );
+  const [nav, setNav] = useState('show');
+
+  const styleComponents = () => {
+    const icon = dynamicBackground
+      ? weatherData.current?.weather[0]?.icon
+      : 'default';
+
+    dispatch(setBackgroundConfig(icon));
+    dispatch(setBackgroundFilter(icon));
+    dispatch(setDashboardStyle(icon));
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       if (lat && lon) {
         const weatherData = await getWeatherData(lat, lon, unit);
-        setWeatherData(weatherData);
+        dispatch(setWeather(weatherData));
         console.log('API called');
       }
     };
 
-    // console.log(weatherData);
     fetchData();
-  }, [lat, lon, unit]);
+  }, [dispatch, lat, lon, unit]);
+
+  useEffect(() => {
+    styleComponents();
+  }, [dispatch, weatherData, dynamicBackground]);
 
   useEffect(() => {
     showNavbar === true ? setNav('show') : setNav('hide');
   }, [showNavbar]);
 
-  const backgroundImage = {
-    backgroundImage: dynamicBackground
-      ? `url(${getImage(weatherData.current?.weather[0]?.icon || 'default')})`
-      : `url(${getImage('default')}`,
-    backgroundSize: 'cover',
-  };
-
-  const backgroundFilter = {
-    backgroundColor: dynamicBackground
-      ? setFilter(weatherData.current?.weather[0]?.icon || 'default')
-      : setFilter('default'),
-  };
-
-  const dashboardStyle = {
-    background: dynamicBackground
-      ? setDashboardColor(weatherData.current?.weather[0]?.icon || 'default')
-      : setDashboardColor('default'),
-  };
-
   return (
     <div className='main-container'>
-      <div className='background-container' style={backgroundImage}>
+      <div className='background-container' style={backgroundConfig}>
         <div className='background-filter' style={backgroundFilter}></div>/
       </div>
 
       <div className={`navbar-container ${nav}`}>
-        <Navbar
-          showNavbar={showNavbar}
-          setShowNavbar={setShowNavbar}
-          setCurrentPage={setCurrentPage}
-        />
+        <Navbar />
       </div>
 
       <div className='page-container'>
         <div className='top-bar-container'>
-          <SearchBar
-            location={location}
-            setLocation={setLocation}
-            setLat={setLat}
-            setLon={setLon}
-            showNavbar={showNavbar}
-            setShowNavbar={setShowNavbar}
-          />
+          <SearchBar />
 
           <div className={`mobile-navbar-container ${nav}`}>
-            <Navbar
-              showNavbar={showNavbar}
-              setShowNavbar={setShowNavbar}
-              setCurrentPage={setCurrentPage}
-            />
+            <Navbar />
           </div>
         </div>
 
-        {currentPage === 'settings' && (
-          <Settings dashboardStyle={dashboardStyle} />
-        )}
+        {currentPage === 'settings' && <Settings />}
 
         {currentPage === 'saved location' && (
-          <SavedLocation
-            weatherData={weatherData}
-            location={location}
-            lat={lat}
-            lon={lon}
-            setLat={setLat}
-            setLon={setLon}
-            dashboardStyle={dashboardStyle}
-          />
+          <SavedLocation dashboardStyle={dashboardStyle} />
         )}
 
         {currentPage === 'dashboard' && (
           <Dashboards
             weatherData={weatherData}
             location={location}
-            setLocation={setLocation}
-            setLat={setLat}
-            setLon={setLon}
             showNavbar={showNavbar}
             setShowNavbar={setShowNavbar}
             setCurrentPage={setCurrentPage}
