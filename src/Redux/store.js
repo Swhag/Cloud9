e5560ee60fdc241e0a5e02fe748da891741d1ade
Swhag <khg5293@outlook.com>
2023-03-30 23:52:33 -0400

    added weatherSlince.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import weatherSlice from './weatherSlice';
 import savedLocationSlice from './savedLocationSlice';
 import settingSlice from './settingSlice';
 
 const store = configureStore({
   reducer: {
+    weather: weatherSlice.reducer,
     savedLocations: savedLocationSlice.reducer,
     settings: settingSlice.reducer,
   },
