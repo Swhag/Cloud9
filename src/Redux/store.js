edbf4effeb485b8e18439f8031db5c5f75b005fc
Swhag <khg5293@outlook.com>
2023-03-31 01:38:10 -0400

    componentStyling states now managed globally using redux

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import weatherSlice from './weatherSlice';
+import componentStylesSlice from './componentStylesSlice';
 import savedLocationSlice from './savedLocationSlice';
 import settingSlice from './settingSlice';
 
 const store = configureStore({
   reducer: {
     weather: weatherSlice.reducer,
+    componentStyles: componentStylesSlice.reducer,
     savedLocations: savedLocationSlice.reducer,
     settings: settingSlice.reducer,
   },
