61eb414d207e91554d3e90ef82866f950f90df81
Swhag <khg5293@outlook.com>
2023-04-01 00:28:17 -0400

    page renamed to weather.js

--- src/pages/Weather.js
+++ src/pages/Weather.js
@@ -1,15 +1,12 @@
 import React, { useState, useEffect } from 'react';
-
 import CurrentWeather from '../components/currentWeather/CurrentWeather';
 import CurrentDetails from '../components/currentWeather/CurrentDetails';
 import WeatherChart from '../components/currentWeather/WeatherChart';
 import HourlyForecast from '../components/HourlyForecast';
 import DailyForecast from '../components/DailyForecast';
-
 import '../styles/dashboards.css';
 
-function Dashboards(props) {
-  const { weatherData, location, setLocation, dashboardStyle } = props;
+function Dashboards() {
   const [isMounted, setIsMounted] = useState(false);
   const [isMounted2, setIsMounted2] = useState(false);
   const [isMounted3, setIsMounted3] = useState(false);
@@ -35,41 +32,22 @@ function Dashboards(props) {
         className={`current-dashboard-container ${isMounted ? 'fade-in' : ''}`}
       >
         <div className='current-dashboard-group'>
-          <CurrentWeather
-            location={location}
-            weatherData={weatherData}
-            dashboardStyle={dashboardStyle}
-          />
-          <CurrentDetails
-            weatherData={weatherData}
-            dashboardStyle={dashboardStyle}
-          />
+          <CurrentWeather />
+          <CurrentDetails />
         </div>
-        <WeatherChart
-          weatherData={weatherData}
-          dashboardStyle={dashboardStyle}
-        />
+        <WeatherChart />
       </div>
 
       <div className='forecast-container'>
         <div
           className={`daily-forecast-container ${isMounted2 ? 'fade-in' : ''}`}
         >
-          <DailyForecast
-            weatherData={weatherData}
-            location={location}
-            setLocation={setLocation}
-            dashboardStyle={dashboardStyle}
-          />
+          <DailyForecast />
         </div>
         <div
           className={`hourly-forecast-container ${isMounted3 ? 'fade-in' : ''}`}
         >
-          <HourlyForecast
-            location={location}
-            weatherData={weatherData}
-            dashboardStyle={dashboardStyle}
-          />
+          <HourlyForecast />
         </div>
       </div>
     </div>
