a6b29f4772ed221866ce3802d7314f76007765c6
Swhag <khg5293@outlook.com>
2023-04-01 00:27:17 -0400

    localStorage added to save user configuration

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,26 @@ import componentStylesSlice from './componentStylesSlice';
 import savedLocationSlice from './savedLocationSlice';
 import settingSlice from './settingSlice';
 
+const saveToLocalStorage = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('cloudnine_settings', serializedState);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const loadFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem('cloudnine_settings');
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+};
+
 const store = configureStore({
   reducer: {
     weather: weatherSlice.reducer,
@@ -11,6 +31,11 @@ const store = configureStore({
     savedLocations: savedLocationSlice.reducer,
     settings: settingSlice.reducer,
   },
+  preloadedState: loadFromLocalStorage(),
+});
+
+store.subscribe(() => {
+  saveToLocalStorage(store.getState());
 });
 
 export default store;
